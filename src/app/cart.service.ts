61e7257e312305ed474f0450d3d4743cc8992825
chowaniecpiotr <chowaniec.piotr@poczta.fm>
2022-08-09 10:57:04 +0200

    Implement cart methods: add, get and clear

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,6 +8,19 @@ import { Product } from './products';
 export class CartService {
 
   items: Product[] = [];
-  
+
   constructor() { }
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
